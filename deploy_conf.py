dedd6ca98e1b4a991cb7d662968031037c319e4c
Yasser Gonzalez Fernandez <ygonzalezfernandez@gmail.com>
2014-10-01 18:00:53 -0400

    Fix error in the config files

--- deploy_conf.py
+++ deploy_conf.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
 
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
+from __future__ import unicode_literals
 
 import os
 import sys
 sys.path.append(os.curdir)
-from pelicanconf import *
+from develop_conf import *
 
 SITEURL = 'http://yglezfdez.com'
+
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -18,7 +17,3 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
--- develop_conf.py
+++ develop_conf.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+
 from __future__ import unicode_literals
 
-AUTHOR = u'Yasser Gonzalez Fernandez'
-SITENAME = u'Personal Website'
+AUTHOR = 'Yasser Gonzalez Fernandez'
+SITENAME = 'Personal Website'
 SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
+# Feed generation disabled when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
@@ -29,5 +30,3 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
